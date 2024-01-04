35f84015175d2e05e8f0048ac3b3b8e6fb432dd7
karimeruvalcaba <137795975+karimeruvalcaba@users.noreply.github.com>
2024-01-04 17:10:41 +0000

    Funcion reflexiva terminada

---
+++ main.py
@@ -0,0 +1,29 @@
+#Karime Itzel Ruvalcaba Pérez A01656188
+
+def buscando_reflexion(relacion):
+    elementos=set()
+    for par in relacion:
+        elementos.add(par[0])
+        elementos.add(par[1])
+
+    for elemento in elementos:
+        if (elemento,elemento) not in relacion:
+            return False
+    return True
+
+def buscando_simetria(relacion):
+    
+      
+def main():
+    
+    relacion={(0,0),(0,1),(0,3),(1,0),(1,1),(2,2),(3,0),(3,3)}
+
+    reflexiva=buscando_reflexion(relacion)
+    symetrica=buscando_simetria(relacion)
+    transitiva=buscando_transicion(relacion)
+
+    print(f"Reflexiva: {reflexiva}")
+
+if __name__ == '__main__':
+    main()
+

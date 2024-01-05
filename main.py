711ebf12845f905452d7a905fa279e071f3d573e
karimeruvalcaba <137795975+karimeruvalcaba@users.noreply.github.com>
2024-01-05 22:01:10 +0000

    Doing the graph

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ast
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
 
 def analyze(val):
@@ -13,24 +14,68 @@ def analyze(val):
 
     return Reflexive,Symmetric,Transitive
 
-def plot():
+def plot(val):
     """
-    Here goes your code to do the plot of the set
+    Here goes your code to generate graph.log and plot the graph
     """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+
+    # Create the graph
+    g = graphviz.Digraph('G', filename='graph.log')
+    g.attr(rankdir='LR', size='8,5')
+
+    for i in val:
+        g.edge(str(i[0]), str(i[1]))
+
+    # Render the graph
     g.view()
+
+    # Create the graph
+    g1 = graphviz.Digraph('G', filename='hello.gv')
+    g1.attr(rankdir='LR', size='8,5')
+
+    for i in val:
+        g1.edge(str(i[0]), str(i[1]))
+
+    # Render the graph
+    g1.view()
+
+
+def get_set(val):
+    """
+    Here goes your code to get the set from the input
+    Example: { (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
+    { (0,0), (1,1), (1,0) }
+    """
+    result = None
+
+    try:
+        # Use literal_eval to convert the string into a set
+        result = ast.literal_eval(val)
+
+        # Check if result is a set
+        if not isinstance(result, set):
+            raise ValueError("Input string does not represent a set")
+
+    except ValueError as e:
+        print(f"Error: Invalid input. {e}")
+        raise e
+    except Exception as e:
+        print("Error: Something went wrong.")
+        raise e
+
+    return result
 
 def main():
     print("Hello World analyzing input!")
     val = input("Enter your set: ")
+    val=get_set(val)
     print(val)
     Reflexive,Symmetric,Transitive = analyze(val)
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+    plot(val)
 
 if __name__ == "__main__":
     main()
